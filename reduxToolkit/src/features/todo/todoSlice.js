4b4ff56b3669efde604890326ccf29bfd1d10171
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2024-06-06 22:38:58 +0530

    add codes

---
+++ reduxToolkit/src/features/todo/todoSlice.js
@@ -0,0 +1,30 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit"
+
+const initialState = {
+    todos: [{ id: 1, text: "Hello World" }]
+
+}
+
+export const todoSlice = createSlice({
+
+    name: 'todo',
+    // we can declare the initial state here also
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+
+
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter(() => todo.id !== action.payload)
+        },
+
+        //update todo i have to do for revision------
+
+    }
+})
