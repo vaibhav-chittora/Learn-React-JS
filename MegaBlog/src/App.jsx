bd9b152ba511542b25afcad88e49607531645c15
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2024-06-25 23:26:16 +0530

    redux configuration

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,15 +1,46 @@
-
+import { useDispatch } from 'react-redux'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { authService } from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import { Footer } from './components';
+import { Outlet } from 'react-router-dom';
+
+
 
 function App() {
-  
-console.log(import.meta.env.VITE_APPWRITE_URL);
 
-  return (
-   <>
-   <h1>A Blogging Platform</h1>
-   </>
-  )
+  const [loading, setLoading] = useState(true);
+  const [dispatch] = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        }else{
+          dispatch(logout())
+        }
+      })
+
+      // .catch() ---> we can imporve the produce using .catch(), if we want
+
+      .finally(()=>setLoading(false))
+  }, [])
+
+ return !loading ? (
+  <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+    <div className='block w-full'>
+      <Header/>
+      <main>
+
+        TODO : {/* <Outlet/> */}
+      </main>
+      <Footer/>
+
+    </div>
+  </div>
+ ):null
 }
 
 export default App
