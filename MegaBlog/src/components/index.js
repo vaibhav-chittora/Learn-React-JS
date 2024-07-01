6cfe3416a4a8fb3c95183a23cf25bb31c8168161
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2024-07-01 16:41:56 +0530

    Adding form and slug values

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -3,5 +3,7 @@ import Footer from "./Footer/Footer";
 import Container from './container/Container'
 import Logo from "./Logo";
 import logoutBtn from './Header/LogoutBtn'
+import RTE from "./RTE";
 
-export { Header, Footer, Container, Logo, logoutBtn }
+
+export { Header, Footer, Container, Logo, logoutBtn, RTE }
