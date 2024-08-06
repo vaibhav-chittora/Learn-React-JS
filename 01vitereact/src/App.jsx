dc8f7396ce9883f4dc40e13f85bbbe53e43ec7d8
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2024-08-06 23:57:42 +0530

    tailwind Setup

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,13 +1,19 @@
 
+import Button from "./Button"
 import Chai from "./chai"
 
 function App() {
-  
+
   return (
     <>
 
-    <h1>Cahi aur react</h1>
-  <Chai/>
+      <h1>Cahi aur react</h1>
+      <Chai />
+      <Button text={'Submit'} buttonType={'submit'} buttonStyle={}/>
+      <Button text={'Click Me'} buttonType={'reset'}/>
+      <Button text ={'Btn'}/>
+     
+     
     </>
   )
 }
